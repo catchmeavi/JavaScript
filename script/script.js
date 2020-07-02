42675bfaf2e25a1ec27b55d08ad4d07f74df3166
Avijit Chakraborty <catchmeavi@gmail.com>
2020-07-01 22:25:48 -0400

    with concat

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function concatArray(){
+  var hege = ["Cecilie", "Lone"];
+  var stale = ["Emil", "Tobias", "Linus"];
+  var children = hege.concat(stale);
+  document.getElementById("resut").innerHTML = children;
+}
