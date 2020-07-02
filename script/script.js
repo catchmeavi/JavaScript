dafb8c1ba8fb540e42c61c5eee0ba564c0bdde54
Avijit Chakraborty <catchmeavi@gmail.com>
2020-07-01 22:42:17 -0400

    added spread operator

--- script/script.js
+++ script/script.js
@@ -4,3 +4,16 @@ function concatArray(){
   var children = hege.concat(stale);
   document.getElementById("resut").innerHTML = children;
 }
+function getSummery(){
+  const person = { name: 'David Walsh', gender: 'Male' };
+  const tools = { computer: 'Mac', editor: 'Atom' };
+
+  const summary = {...person, ...tools};
+  document.getElementById("resut1").innerHTML = JSON.stringify(summary);
+} 
+function concatArraySpread(){
+  var hege = ["Cecilie", "Lone"];
+  var stale = ["Emil", "Tobias", "Linus"];
+  var children = [...hege,...stale ];
+  document.getElementById("resut3").innerHTML = children;
+}
